4a8f4374ff31c96492d335ab2a7bcb2c139231c1
Marzieh <mar.31069@gmail.com>
2021-09-24 22:49:08 +0330

    Add JavaScript

---
+++ Day 10 - Hold Shift and Check Checkboxes/main.js
@@ -0,0 +1,31 @@
+function main() {
+  const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+  let lastChecked;
+
+  function handleCheck(e) {
+
+    // Check if they had the shift key down
+    // And check that they are checking it
+    let inBetween = false;
+    if(e.shiftKey && this.checked) {
+      // go ahead and do what we please
+      checkboxes.forEach(checkbox => {
+        if (checkbox === this || checkbox === lastChecked) {
+          inBetween = !inBetween;
+        }
+
+        if (inBetween) {
+          checkbox.checked = true;
+        }
+      });
+    }
+    
+    lastChecked = this;
+    
+  }
+
+  checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+}
+
+main();
