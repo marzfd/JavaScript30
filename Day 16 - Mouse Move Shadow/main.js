9d4a7bdfe1cd617fd46ba7316b5c894687f4fb5b
Marzieh <mar.31069@gmail.com>
2021-10-07 15:00:08 +0330

    Add JavaScript

---
+++ Day 16 - Mouse Move Shadow/main.js
@@ -0,0 +1,33 @@
+function main() {
+
+  const hero = document.querySelector('.hero');
+  const text = document.querySelector('h1');
+  const walk = 500; // 500px
+
+
+  function shadow(e) {
+
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+
+    if (this !== e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    text.style.textShadow = `
+                            ${xWalk}px ${yWalk}px 0 #00EAD399,
+                            ${xWalk * -1}px ${yWalk}px 0 #FFF5B799,
+                            ${yWalk}px ${xWalk * -1}px 0 #FF449F99,
+                            ${yWalk * -1}px ${xWalk}px 0 #005F9999
+                          `;
+
+  }
+
+  hero.addEventListener('mousemove', shadow);
+}
+
+main();
